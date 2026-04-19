37c96f918cb6c404060deaf0f834e785406c9361
prernak199-create <prernak199@gmail.com>
2026-04-19 22:55:05 +0200

    Update ddl_silver.sql
    Removed various data quality checks and standardization queries for silver and bronze tables.

--- scripts/silvers/ddl_silver.sql
+++ scripts/silvers/ddl_silver.sql
@@ -41,72 +41,6 @@ WHERE cst_id IS NOT NULL
 )t WHERE flag_last =1 ;
 
 
---CHECK QUALITY OF SILVER
---Re-run the quality check queries from the bronze layer to varify the quality of the data in silver layer.
-
---Check for null or duplicates in primary key
-SELECT cst_id,COUNT(*)
-FROM silver.crm_cust_info
-GROUP BY cst_id 
-HAVING COUNT(*)>1 OR cst_id IS NULL
-
-
---Check for spaces in string
---Expectation:No Result
-SELECT cst_firstname 
-from silver.crm_cust_info
-where cst_firstname !=TRIM(cst_firstname)
-
---if the original value is not equalto the same value after triming it means there are spaces
---CHECK FOR UNWANTED SPACES IN LASTNAME
---EXPECTSTION=No Result
-SELECT cst_lastname
-FROM silver.crm_cust_info
-where cst_lastname !=TRIM(cst_lastname)
-
---FOR CHECKING SPACES IN GENDER
-SELECT cst_gndr
-FROM silver.crm_cust_info
-WHERE cst_gndr !=TRIM(cst_gndr)
-
---DATA Standardization OR ABBREVIATION and consistency check
-SELECT  DISTINCT cst_gndr
-FROM silver.crm_cust_info
-
---FOR MARITALSTATUS
-SELECT DISTINCT cst_material_status
-FROM silver.crm_cust_info
-
-SELECT * FROM silver.crm_cust_info
-
---Check for null or duplicate Primary Key in bronze.crm_prd_info table
---Expectation:No Result
-SELECT
-prd_id,COUNT(*)
-FROM bronze.crm_prd_info
-GROUP BY prd_id
-HAVING COUNT(*)>1 OR prd_id IS NULL
-
---CHECK FOR UNWANTED SPACES
-SELECT prd_nm 
-from bronze.crm_prd_info
-where prd_nm !=TRIM(prd_nm)
-
---check for nulls or negative numbers
---expectation:No result
-SELECT prd_cost
-FROM bronze.crm_prd_info
-WHERE prd_cost<0 OR prd_cost IS NULL
-
---DATA Standardization OR ABBREVIATION and consistency check
-SELECT  DISTINCT prd_line
-FROM bronze.crm_prd_info
-
---Check for Invalid Date Orders
-SELECT * FROM bronze.crm_prd_info
-WHERE prd_end_dt< prd_start_dt
-
-
 truncate table silver.crm_prd_info;
 INSERT INTO silver.crm_prd_info(
 prd_id,
@@ -134,57 +68,6 @@ CAST(prd_start_dt AS DATE)AS prd_start_dt,
 CAST(LEAD(prd_start_dt)OVER(PARTITION BY prd_key 
 ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
 FROM bronze.crm_prd_info;
-
---check for silver.crm_prd_info
---DATA Standardization OR ABBREVIATION and consistency check
-SELECT  DISTINCT prd_line
-FROM silver.crm_prd_info
---Check for Invalid Date Orders
-SELECT * FROM silver.crm_prd_info
-WHERE prd_end_dt< prd_start_dt
---
-select * from silver.crm_prd_info
-
---Check for INVALID DATES that is negative or zero in bronze.crm_sales_details
---zero is replaced by NULL and length must be 8
-SELECT 
-NULLIF(sls_due_dt,0)sls_due_dt
-FROM bronze.crm_sales_details
-WHERE sls_due_dt<=0
-OR LEN(sls_due_dt)!=8
-OR sls_due_dt>20500101
-OR sls_due_dt<19000101
-
---Check for Invalid Date Orders
-select * from bronze.crm_sales_details
-where sls_order_dt> sls_ship_dt OR sls_order_dt> sls_due_dt
-
---CHECK DATA CONSISTENCY BETWEEN: Sales,Quantity ,Price
--->> Sales = Quantity * Price
--->> Values must not be -ve,null,or zero
-
-Select  DISTINCT sls_sales AS old_sls_sales,
-sls_quantity,
-sls_price as old_sls_price,
-
-CASE WHEN sls_sales IS NULL OR sls_sales <=0 OR sls_sales!=sls_quantity*ABS(sls_price)
-     THEN sls_quantity* ABS(sls_price)
-     ELSE sls_sales
-     END as sls_sales,
-
-CASE WHEN sls_price IS NULL OR sls_price <=0
-     THEN sls_price / NULLIF(sls_quantity,0)
-     ELSE sls_price
-     END as sls_price
-from bronze.crm_sales_details
-where sls_sales != sls_quantity * sls_price
-OR sls_sales IS NULL 
-or sls_quantity is null
-or sls_price  is null
-OR sls_sales <=0
-or sls_quantity<=0
-or sls_price<=0;
-
 
 --transformation of table
 TRUNCATE TABLE silver.crm_sales_details;
@@ -225,25 +108,6 @@ CASE WHEN sls_price IS NULL OR sls_price <=0
      END as sls_price
 FROM bronze.crm_sales_details;
 
---Check for Invalid Date Orders
-select * from silver.crm_sales_details
-where sls_order_dt> sls_ship_dt OR sls_order_dt> sls_due_dt;
---CHECK DATA CONSISTENCY BETWEEN: Sales,Quantity ,Price
--->> Sales = Quantity * Price
--->> Values must not be -ve,null,or zero
-Select  DISTINCT sls_sales,
-sls_quantity,
-sls_price 
-from silver.crm_sales_details
-where sls_sales != sls_quantity * sls_price
-OR sls_sales IS NULL 
-or sls_quantity is null
-or sls_price  is null
-OR sls_sales <=0
-or sls_quantity<=0
-or sls_price<=0
-ORDER BY sls_sales,sls_quantity,sls_price;
-select * from silver.crm_sales_details;
 
 --transformation of silver.erp_cust_az12
 truncate table silver.erp_cust_az12;
@@ -263,23 +127,6 @@ END AS bdate,
  ELSE 'N/A'
  END AS gen
 from bronze.erp_cust_az12 ;
---DATA QUALITY CHECK OF bronze.erp_cust_az12
---Identify Out of Range Dates Very old Customer
-SELECT bdate from bronze.erp_cust_az12
-WHERE bdate<'1924-01-01' OR 
- bdate > GETDATE()
- --Data STANDARDIZATION
- SELECT DISTINCT gen
- from bronze.erp_cust_az12
- --DATA QUALITY CHECK OF silver.erp_cust_az12
- --Identify Out of Range Dates Very old Customer
-SELECT bdate from silver.erp_cust_az12
-WHERE bdate<'1924-01-01' OR 
- bdate > GETDATE()
- --Data STANDARDIZATION
- SELECT DISTINCT gen
- from silver.erp_cust_az12;
- SELECT* FROM silver.erp_cust_az12;
 
 --transformation of erp_loc
 TRUNCATE TABLE silver.erp_loc_a101;
@@ -295,14 +142,6 @@ else cntry
 END AS cntry
 FROM bronze.erp_loc_a101;
 
---checking
-SELECT cst_key from silver.crm_cust_info
---check for standardization
-select distinct cntry from bronze.erp_loc_a101
-order by cntry;
---check in silver
-select distinct cntry from silver.erp_loc_a101;
-SELECT* FROM silver.erp_loc_a101;
 
 --transforming
 TRUNCATE TABLE silver.erp_px_cat_g1v2
@@ -314,19 +153,9 @@ cat,
 subcat,
 maintenance
 from bronze.erp_px_cat_g1v2;
-
 -------------
 select* from silver.erp_px_cat_g1v2;
 
---check Quality
----check for unwanted spaces
-select * from bronze.erp_px_cat_g1v2
-where cat!=TRIM(cat) or subcat!=TRIM(subcat)
-or maintenance!=TRIM(maintenance)
---DATA STANDARDISATION CHECK
-SELECT DISTINCT cat from bronze.erp_px_cat_g1v2;
-select distinct subcat from bronze.erp_px_cat_g1v2;
-SELECT distinct maintenance from bronze.erp_px_cat_g1v2;
 
 
 
