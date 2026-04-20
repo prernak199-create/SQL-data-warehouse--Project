8085cf4a331efda582845f5ce27c8f5ce55bfd4b
prernak199-create <prernak199@gmail.com>
2026-04-20 18:29:04 +0200

    Update quality_checks_gold.sql

--- scripts/tests/quality_checks_gold.sql
+++ scripts/tests/quality_checks_gold.sql
@@ -1,1 +1,18 @@
 
+--checking for Standardisation and consistency 
+select distinct gender from gold.dim_customers;
+---------------------------------------------------------------------------------------------------------------
+select distinct ci.cst_gndr,
+ca.gen,
+CASE WHEN ci.cst_gndr!='n/a' then ci.cst_gndr --CRM is the Master for gender info
+ELSE COALESCE(ca.gen,'n/a')
+END AS new_gen
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key= ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+on ci.cst_key=la.cid
+ORDER BY
+1,2;
+
+
